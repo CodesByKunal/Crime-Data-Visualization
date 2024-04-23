75b4a15e44d533fc637f4f331114885ea7155e81
CodesByKunal <8055kunalkumar@gmail.com>
2024-04-23 17:30:31 +0530

    Error Handle

--- Backend/Middlewares/verifyUser.js
+++ Backend/Middlewares/verifyUser.js
@@ -3,14 +3,16 @@ import jwt from "jsonwebtoken";
 
 const verifyUser = async (req, res, next) => {
     try {
-
+        // | Check for access_token in req.cookies
         const access_token = req.signedCookies.access_token;
         if (!access_token) {
             return res.status(401).send('Not Allowed');
         }
 
+        // | Decode the access_token
         const decoded = await jwt.verify(access_token, 'shhhhh');
 
+        // | Find user in db with id
         const user = await User.findById(decoded._id);
         if (!user.admin) {
             return res.status(401).send('Not Allowed');
@@ -18,7 +20,10 @@ const verifyUser = async (req, res, next) => {
 
         next();
     } catch (error) {
-        console.error('verifyUser', error)
+
+        console.error('verifyUser', error);
+        res.status(500).json({ error: 'Something went wrong' });
+
     }
 
 }
